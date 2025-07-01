83bb3bb187342b07ae407258540b0d70def9785d
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-07-01 16:48:35 +0300

    feat: add front-page template to theme

---
+++ pages/front-page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Front Page
+ *
+ * @package domca
+ */
+
+get_header();
+?>
+
+<div class="relative z-20 overflow-x-hidden">
+	<?php the_content(); ?>
+</div>
+
+<?php get_footer(); ?>
