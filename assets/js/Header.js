91c1694cff80f1d2438d049425fd8272112b34ea
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-07-01 16:51:48 +0300

    feat: implement header component with styles and scroll functionality

---
+++ assets/js/Header.js
@@ -0,0 +1,78 @@
+class Header {
+	headerEl = document.querySelector('header#dm-header');
+	scrollPrev = 0;
+
+	constructor() {
+		if (!this.headerEl) return;
+
+		this.boundScrollHandler = this.scrollHandler.bind(this);
+
+		this.menu = this.headerEl.querySelector('#primary-menu');
+
+		this.mobBurgerBtn = document.querySelector('.mob-burger-btn');
+		this.boundDisplayMobMenuHandler = this.displayMobMenu.bind(this);
+		this.languageSwitcher = document.querySelector('.dm-languages');
+
+		this.init();
+	}
+
+	init() {
+		this.boundScrollHandler();
+		window.addEventListener('scroll', this.boundScrollHandler);
+
+		if (this.mobBurgerBtn) {
+			this.mobBurgerBtn.addEventListener(
+				'click',
+				this.boundDisplayMobMenuHandler,
+			);
+		}
+
+		if (this.languageSwitcher) {
+			import('./LanguageSwitcher.js').then(
+				({ default: LanguageSwitcher }) =>
+					new LanguageSwitcher(this.languageSwitcher),
+			);
+		}
+	}
+
+	isHeaderHide(scrolled) {
+		return scrolled > 100 && scrolled > this.scrollPrev;
+	}
+
+	isHeaderScrolled(scrolled) {
+		return scrolled > 100;
+	}
+
+	scrollHandler() {
+		const scrolled = window.scrollY;
+
+		if (this.isHeaderHide(scrolled)) {
+			this.headerEl.classList.add('dm-out');
+
+			if (this.languageSwitcher.classList.contains('active')) {
+				this.languageSwitcher.classList.remove('active');
+			}
+		} else {
+			this.headerEl.classList.remove('dm-out');
+		}
+
+		if (this.isHeaderScrolled(scrolled)) {
+			this.headerEl.classList.add('dm-scrolled');
+		} else {
+			this.headerEl.classList.remove('dm-scrolled');
+		}
+
+		this.scrollPrev = scrolled;
+	}
+
+	displayMobMenu() {
+		document.body.classList.toggle('mob-menu-active');
+
+		this.mobBurgerBtn.setAttribute(
+			'aria-expanded',
+			document.body.classList.contains('mob-menu-active'),
+		);
+	}
+}
+
+export default Header;
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
-(function () {})();
+(function () {
+	const elements = {
+		header: document.querySelector('#dm-header'),
+	};
+
+	if (elements.header) {
+		import('./Header.js').then(({ default: Header }) => new Header());
+	}
+})();
