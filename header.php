7bb53f6a644a9cb4d1b02a1a72a8e02126abf95c
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-07-01 16:51:13 +0300

    feat: introduce language switcher component with styles and functionality

--- header.php
+++ header.php
@@ -24,5 +24,53 @@ $domca_body_class = 'bg-cream text-charcoal';
 </head>
 <body <?php body_class( $domca_body_class ); ?>>
 <?php wp_body_open(); ?>
-	<header id="ew-header" class=""></header>
 <div class="min-h-screen flex flex-col justify-between">
+	<header
+			id="dm-header"
+			class="dm-wrap md:pt-7.5 pt-5 pb-4 transition-all duration-300 fixed left-0 top-0 z-50 w-full lg:h-auto h-full lg:max-h-none max-h-18 lg:overflow-visible overflow-hidden"
+			role="banner"
+			itemscope
+			itemtype="https://schema.org/WPHeader"
+	>
+		<div class="dm-container w-full lg:flex grid grid-cols-2 gap-y-5 items-center justify-between">
+			<a
+					href="<?php echo esc_attr( get_home_url() ); ?>"
+					class="dm-header__logo"
+					title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>"
+					aria-label="<?php echo esc_attr__( 'Go to homepage', 'dm' ); ?>"
+					itemprop="url"
+			>
+				<figure itemscope itemtype="https://schema.org/ImageObject">
+					<?php get_template_part( '/vector-images/logo-header' ); ?>
+				</figure>
+			</a>
+
+			<button class="mob-burger-btn"
+					type="button"
+					aria-expanded="false"
+					aria-controls="mobile-menu"
+					title="<?php echo esc_attr__( 'Show/hide mobile menu', 'dm' ); ?>">
+				<span class="dm-visually-hidden"><?php echo esc_html__( 'Menu', 'dm' ); ?></span>
+				<span class="mob-burger-btn-top"></span>
+				<span class="mob-burger-btn-center"></span>
+				<span class="mob-burger-btn-bottom"></span>
+			</button>
+
+			<div class="lg:flex grid lg:items-center lg:justify-end xl:gap-10 lg:gap-5 gap-10 col-span-full">
+				<?php
+				if ( has_nav_menu( 'header-menu' ) ) {
+					wp_nav_menu(
+						array(
+							'theme_location' => 'header-menu',
+							'menu_id'        => 'dm-header-menu',
+							'menu_class'     => 'dm-nav-menu flex items-center',
+							'container'      => 'nav',
+						)
+					);
+				}
+				?>
+
+				<?php get_template_part( '/template-parts/language-switcher' ); ?>
+			</div>
+		</div>
+	</header>
---
+++ template-parts/language-switcher.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * An array that stores a list of languages.
+ *
+ * This variable is used to hold a collection of language names,
+ * which can be utilized for functionalities such as language selection,
+ * displaying supported languages, or performing language-specific
+ * operations.
+ *
+ * @package domca
+ */
+
+// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+$domca_languages = apply_filters( 'wpml_active_languages', null, array( 'skip_missing' => 0 ) );
+
+$domca_languages_base_class = 'dm-languages';
+
+if ( ! empty( $domca_languages ) ) :
+	?>
+	<div
+			class="<?php echo esc_attr( $domca_languages_base_class ); ?>"
+			aria-label="<?php echo esc_attr__( 'Language switcher', 'dm' ); ?>"
+	>
+		<div class="<?php echo esc_attr( $domca_languages_base_class . '-active' ); ?>">
+			<?php foreach ( $domca_languages as $domca_lang ) : ?>
+				<?php
+				if ( $domca_lang['active'] ) {
+					if ( ! empty( $domca_lang['country_flag_url'] ) ) {
+						echo '<span class="wpml-lang-code" lang="' . esc_attr( $domca_lang['language_code'] ) . '">' . esc_html( strtoupper( $domca_lang['language_code'] ) ) . '</span>';
+						echo '<img src="' . esc_url( $domca_lang['country_flag_url'] ) . '" width="18" height="12" loading="lazy" alt="' . esc_attr( $domca_lang['translated_name'] ) . '" class="wpml-flag">';
+					}
+				}
+			endforeach;
+			?>
+		</div>
+		<div class="<?php echo esc_attr( $domca_languages_base_class . '-list-wrap' ); ?>">
+			<ul class="<?php echo esc_attr( $domca_languages_base_class . '-list' ); ?>">
+				<?php
+				foreach ( $domca_languages as $domca_lang ) :
+					if ( ! $domca_lang['active'] ) :
+						if ( ! empty( $domca_lang['country_flag_url'] ) ) :
+							?>
+							<li>
+								<a
+										href="<?php echo esc_url( $domca_lang['url'] ); ?>"
+										title="<?php echo esc_attr( $domca_lang['translated_name'] ); ?>"
+										lang="<?php echo esc_attr( $domca_lang['language_code'] ); ?>"
+								>
+									<span><?php echo esc_html( strtoupper( $domca_lang['language_code'] ) ); ?></span>
+									<img
+											src="<?php echo esc_url( $domca_lang['country_flag_url'] ); ?>"
+											alt="<?php echo esc_attr( $domca_lang['translated_name'] ); ?>"
+											loading="lazy"
+											width="18"
+											height="12"
+											class="wpml-flag"
+									>
+								</a>
+							</li>
+							<?php
+						endif;
+					endif;
+				endforeach;
+				?>
+			</ul>
+		</div>
+	</div>
+<?php endif; ?>
