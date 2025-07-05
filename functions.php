d319dc875c61538983f8296ff8f2290579d2c538
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-07-05 20:27:18 +0300

    build: release version 1.0.10

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
  * The domca functions and definitions
  */
 
-define( 'DOMCA_VERSION', '1.0.09' );
+define( 'DOMCA_VERSION', '1.0.10' );
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
