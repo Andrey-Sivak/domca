c34b56eb4527c2a7bae8b4e6c4b8f485c080322c
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-07-12 11:59:04 +0300

    feat(pages): add new template for Safe Circle Club page

---
+++ pages/safe-circle-club.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Safe Circle Club
+ *
+ * @package domca
+ */
+
+get_header();
+?>
+
+<div class="relative z-20 dm-safe-circle-club">
+	<?php the_content(); ?>
+</div>
+
+<?php get_footer(); ?>
