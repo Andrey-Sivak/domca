25df29b1db313b1fffee316d25d79cb8998df3d3
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-07-13 16:39:07 +0300

    feat(pages): add Support Zone template

---
+++ pages/support-zone.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Support Zone
+ *
+ * @package domca
+ */
+
+get_header();
+?>
+
+<div class="relative z-20">
+	<?php the_content(); ?>
+</div>
+
+<?php get_footer(); ?>
