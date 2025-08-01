735765530116baf5c19a1a01d1f4bf3551575061
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-08-01 18:03:22 +0300

    feat(theme): add About Me page template

---
+++ pages/about.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: About Me
+ *
+ * @package domca
+ */
+
+get_header();
+?>
+
+<div class="relative z-20 dm-about-page">
+	<?php the_content(); ?>
+</div>
+
+<?php get_footer(); ?>
