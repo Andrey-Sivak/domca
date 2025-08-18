ea36d6d890c9c99d9557d5bbe486d20fa0c40f54
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-08-18 12:40:37 +0300

    chore(theme): bump version to 1.0.136

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
  * The domca functions and definitions
  */
 
-define( 'DOMCA_VERSION', '1.0.134' );
+define( 'DOMCA_VERSION', '1.0.136' );
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
