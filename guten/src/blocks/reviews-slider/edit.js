26432a0a473abf93102c90aa08bb74de13bb8622
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-08-31 01:03:19 +0300

    feat(blocks): enhance Reviews Slider block with subtitle and CTA button functionality, new styles, and SVG clip integration

--- guten/src/blocks/reviews-slider/edit.js
+++ guten/src/blocks/reviews-slider/edit.js
@@ -1,11 +1,17 @@
-import { useBlockProps, RichText } from '@wordpress/block-editor';
-import './editor.scss';
+import {
+	useBlockProps,
+	InspectorControls,
+	RichText,
+} from '@wordpress/block-editor';
 import { Fragment } from '@wordpress/element';
+import { PanelBody, CheckboxControl } from '@wordpress/components';
 import ReviewList from './ReviewList.js';
+import './editor.scss';
 
 const Edit = (props) => {
 	const { attributes, setAttributes } = props;
-	const { title, subtitle, items } = attributes;
+	const { title, useSubtitle, subtitle, items, useButton, button } =
+		attributes;
 
 	const baseClass = 'wp-block-domca-reviews-slider';
 
@@ -13,8 +19,32 @@ const Edit = (props) => {
 		className: baseClass + ' dm-wrap',
 	});
 
+	const onChangeUseButton = (newValue) => {
+		setAttributes({ useButton: newValue });
+	};
+
+	const onChangeUseSubtitle = (newValue) => {
+		setAttributes({ useSubtitle: newValue });
+	};
+
 	return (
 		<Fragment>
+			<InspectorControls>
+				<PanelBody title="Block Settings" initialOpen={true}>
+					<CheckboxControl
+						label="Use CTA Button"
+						help=""
+						checked={useButton}
+						onChange={onChangeUseButton}
+					/>
+					<CheckboxControl
+						label="Use Subtitle"
+						help=""
+						checked={useSubtitle}
+						onChange={onChangeUseSubtitle}
+					/>
+				</PanelBody>
+			</InspectorControls>
 			<div {...blockProps}>
 				<div className={`${baseClass}__wrap dm-container`}>
 					<RichText
@@ -26,21 +56,37 @@ const Edit = (props) => {
 						}
 						placeholder="Input section title..."
 					/>
-					<RichText
-						tagName="p"
-						className={`${baseClass}__subtitle`}
-						value={subtitle}
-						onChange={(newSubtitle) =>
-							setAttributes({ subtitle: newSubtitle })
-						}
-						placeholder="Input section subtitle..."
-					/>
+					{useSubtitle && (
+						<RichText
+							tagName="p"
+							className={`${baseClass}__subtitle`}
+							value={subtitle}
+							onChange={(newSubtitle) =>
+								setAttributes({ subtitle: newSubtitle })
+							}
+							placeholder="Input section subtitle..."
+						/>
+					)}
 
 					<ReviewList
 						items={items}
 						setAttributes={setAttributes}
 						baseClass={baseClass}
 					/>
+					{useButton && (
+						<RichText
+							tagName="p"
+							className={`${baseClass}__button dm-button dm-button-white`}
+							value={button}
+							onChange={(newButton) =>
+								setAttributes({
+									button: newButton,
+								})
+							}
+							placeholder="Button text..."
+							allowedFormats={['core/link']}
+						/>
+					)}
 				</div>
 			</div>
 		</Fragment>
